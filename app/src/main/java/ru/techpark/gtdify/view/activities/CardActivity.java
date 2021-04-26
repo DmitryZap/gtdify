f2e3dab2420ca15bf0082e3b34225ec62ed5d911
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 <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>
2021-04-26 18:22:19 +0300

    card activity + datapicker in card + style colors fixes

--- app/src/main/java/ru/techpark/gtdify/view/activities/CardActivity.java
+++ app/src/main/java/ru/techpark/gtdify/view/activities/CardActivity.java
@@ -1,13 +1,17 @@
 package ru.techpark.gtdify.view.activities;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Calendar;
 
 import ru.techpark.gtdify.R;
 
@@ -19,11 +23,7 @@ public class CardActivity extends AppCompatActivity {
         setContentView(R.layout.card_activity);
 
         initToolbar();
-
-        FloatingActionButton acceptButton = findViewById(R.id.accept_button);
-        acceptButton.setOnClickListener(v -> {
-
-        });
+        initSelectDialog();
     }
 
     private void initToolbar() {
@@ -33,6 +33,16 @@ public class CardActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         toolbar.setNavigationOnClickListener(v -> onBackPressed());
+    }
+
+    private void initSelectDialog() {
+        View selectData = findViewById(R.id.selectData);
+        Calendar dateAndTime = Calendar.getInstance();
+        selectData.setOnClickListener(v -> {
+            new DatePickerDialog(this, (DatePickerDialog.OnDateSetListener) (datePicker, i, i1, i2) -> {
+
+            }, dateAndTime.get(Calendar.YEAR), dateAndTime.get(Calendar.MONTH), dateAndTime.get(Calendar.DAY_OF_MONTH)).show();
+        });
     }
 
 }
